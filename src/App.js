66c329c411dc75f584121ab1fe3da464af040fc5
Anna Frick <ae224pc@student.lnu.se>
2025-01-02 08:50:29 +0100

    Setting up website

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="/ror.png" className="App-logo" alt="logo" />
+        <p>
+          Peter Engström Rörtjänst AB, utför VS-jobb hos dig! 
+        </p>
+      </header>
+      <p>
+          Peter Engström Rörtjänst AB utför VS-jobb i din bostad som installation/justering av värmesystem och värmepump, renovering/ nybyggnation av badrum, kök och tvättstuga. Alltid med noggrannhet och kvalitet enligt SÄKER VATTEN-certifiering. Med ROT-subvention.
+      </p>
+      <img src="/sakervatten.png" className="" alt="Saker vatten" />
+    </div>
+  );
+}
+
+export default App;
