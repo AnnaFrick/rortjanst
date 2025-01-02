9558263f96613620efaf6f33d1481be749ea1c40
Anna Frick <ae224pc@student.lnu.se>
2025-01-02 10:22:18 +0100

    Styling to links

--- src/About.js
+++ src/About.js
@@ -1,35 +1,46 @@
 import './About.css';
+import { FaPhoneAlt, FaEnvelope } from "react-icons/fa";
 
 function About() {
-  return (
-    <div className="About">
-      <h1>Om Peter Engström Rörtjänst AB</h1>
-      <p>
-        Peter Engström Rörtjänst AB är ett Kalmar-baserat VVS-företag som erbjuder en rad tjänster för ditt hem. Med noggrannhet och kvalitet har företaget utfört VS-jobb i över 20 år. Tjänsterna inkluderar:
-      </p>
-      <ul>
-        <li>Installation av värmesystem och värmepumpar</li>
-        <li>Badrum-, köks- och tvättstuge-renoveringar</li>
-        <li>Rördragning och VVS-arbeten</li>
-      </ul>
-      <p>
-        Peter Engström Rörtjänst AB är certifierade enligt Säker Vatten, vilket innebär att företaget uppfyller branschens krav på kvalitet och säkerhet. Med ROT-avdrag kan du få skatteavdrag på arbetskostnaden.
-      </p>
-        <p>
-            Kontakta oss för mer information och offertförfrågan.
-        </p>
-        <h2>Kontakt</h2>
-        <p>070-698 38 38</p>
-        <p>peterengstrom@telia.com</p>
+    return (
+        <div className="About">
+            <h1>Om Peter Engström Rörtjänst AB</h1>
+            <p>
+                Peter Engström Rörtjänst AB är ett Kalmar-baserat VVS-företag som erbjuder en rad tjänster för ditt hem. Med noggrannhet och kvalitet har företaget utfört VS-jobb i över 20 år. Tjänsterna inkluderar:
+            </p>
+            <ul>
+                <li>Installation av värmesystem och värmepumpar</li>
+                <li>Badrum-, köks- och tvättstuge-renoveringar</li>
+                <li>Rördragning och VVS-arbeten</li>
+            </ul>
+            <p>
+                Peter Engström Rörtjänst AB är certifierade enligt Säker Vatten, vilket innebär att företaget uppfyller branschens krav på kvalitet och säkerhet. Med ROT-avdrag kan du få skatteavdrag på arbetskostnaden.
+            </p>
+            <p>
+                Kontakta oss för mer information och offertförfrågan.
+            </p>
+            <h2>Kontakt</h2>
+            <p>
+                <FaPhoneAlt className="about-icon" />
+                <a href="tel:0706983838" className="about-link">
+                    070-698 38 38
+                </a>
+            </p>
+            <p>
+                <FaEnvelope className="about-icon" />
+                <a href="mailto:peterengstrom@telia.com" className="about-link">
+                    peterengstrom@telia.com
+                </a>
+            </p>
 
-        <div className='aboutPictures'>
-            <img src="/1.jpg" alt="Peter" />
-            <img src="/2.jpg" alt="Arbete" />
-            <img src="/3.jpg" alt="Arbete" />
-            <img src="/4.jpg" alt="Företagsbil" />
+            <div className='aboutPictures'>
+                <img src="/1.jpg" alt="Peter" />
+                <img src="/2.jpg" alt="Arbete" />
+                <img src="/3.jpg" alt="Arbete" />
+                <img src="/4.jpg" alt="Företagsbil" />
+            </div>
         </div>
-    </div>
-  );
+    );
 }
 
 export default About;
--- src/Footer.js
+++ src/Footer.js
@@ -1,13 +1,24 @@
-import "./Footer.css"
+import "./Footer.css";
+import { FaPhoneAlt, FaEnvelope } from "react-icons/fa";
 
 const Footer = () => {
-    return (
-        <div className="footer">
-            <p>© 2024 Peter Engström Rörtjänst AB</p>
-            <p>070-698 38 38</p>
-            <p>peterengstrom@telia.com</p>
-        </div>
-    )
-}
+  return (
+    <div className="footer">
+      <p>© 2024 Peter Engström Rörtjänst AB</p>
+      <p>
+        <FaPhoneAlt className="icon" />
+        <a href="tel:0706983838" className="footer-link">
+          070-698 38 38
+        </a>
+      </p>
+      <p>
+        <FaEnvelope className="icon" />
+        <a href="mailto:peterengstrom@telia.com" className="footer-link">
+          peterengstrom@telia.com
+        </a>
+      </p>
+    </div>
+  );
+};
 
 export default Footer;
