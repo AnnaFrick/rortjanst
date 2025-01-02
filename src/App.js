11165ad3ab37c5a3a15ca8d7e270db3323ccb5bb
Anna Frick <ae224pc@student.lnu.se>
2025-01-02 09:28:22 +0100

    Some styling added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import './App.css';
+import Footer from './Footer';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src="/ror.png" className="App-logo" alt="logo" />
-        <p>
-          Peter Engström Rörtjänst AB, utför VS-jobb hos dig! 
-        </p>
       </header>
-      <p>
-          Peter Engström Rörtjänst AB utför VS-jobb i din bostad som installation/justering av värmesystem och värmepump, renovering/ nybyggnation av badrum, kök och tvättstuga. Alltid med noggrannhet och kvalitet enligt SÄKER VATTEN-certifiering. Med ROT-subvention.
-      </p>
-      <img src="/sakervatten.png" className="" alt="Saker vatten" />
+      <div className="App-body">
+        <h1>Peter Engström Rörtjänst AB</h1>
+        <h2>Utför VS-jobb hos dig!</h2>
+        <p className="App-description">
+          Peter Engström Rörtjänst AB utför VS-jobb i din bostad som installation/justering av värmesystem och värmepump, 
+          renovering/nybyggnation av badrum, kök och tvättstuga. Alltid med noggrannhet och kvalitet enligt 
+          <strong> SÄKER VATTEN-certifiering</strong>. Med ROT-subvention.
+        </p>
+        <img src="/sakervatten.png" height={350} className="App-certification" alt="Säker Vatten Certification" />
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import "./Footer.css"
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <p>© 2024 Peter Engström Rörtjänst AB</p>
+            <p>070-698 38 38</p>
+            <p>peterengstrom@telia.com</p>
+        </div>
+    )
+}
+
+export default Footer;
